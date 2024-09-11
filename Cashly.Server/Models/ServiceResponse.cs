aa6753202028be6852741295a3ffece6b597a596
eddyiiz <eddyxsayf@gmail.com>
2024-09-11 17:35:19 +0200

    model edit

--- Cashly.Server/Models/ServiceResponse.cs
+++ Cashly.Server/Models/ServiceResponse.cs
@@ -4,5 +4,5 @@ public class ServiceResponse<T>
 {
     public T? Data { get; set; }
     public string Message { get; set; } = string.Empty;
-    public bool Successful { get; set; } = true;
+    public bool Success { get; set; } = true;
 }
