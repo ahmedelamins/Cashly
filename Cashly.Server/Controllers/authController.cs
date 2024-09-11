99dea27a9d666953c4a9e5894c41e38e3ab185b6
eddyiiz <eddyxsayf@gmail.com>
2024-09-11 18:26:54 +0200

    auth controller

---
+++ Cashly.Server/Controllers/authController.cs
@@ -0,0 +1,74 @@
+﻿using Cashly.Server.Services.AuthService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cashly.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class authController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public authController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegister request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var response = await _authService
+            .Register(
+             new User
+             {
+                 Username = request.Username
+             },
+                request.Password
+            );
+
+        if (!response.Success)
+        {
+            return BadRequest(response.Message);
+        }
+
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<ServiceResponse<string>>> Login(UserLogin request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var response = await _authService.Login(request.Username, request.Password);
+
+        if (!response.Success)
+        {
+            return BadRequest(response.Message);
+        }
+
+        return Ok(response);
+    }
+
+    [HttpDelete("delete-user"), Authorize]
+    public async Task<ActionResult<ServiceResponse<bool>>> DeleteUser(int userId)
+    {
+        var response = await _authService.DeleteUser(userId);
+
+        if (!response.Success)
+        {
+            return BadRequest(response.Message);
+        }
+
+        return Ok(response);
+    }
+
+}
