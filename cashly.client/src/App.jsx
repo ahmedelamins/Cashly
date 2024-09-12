c9bccbec7dff018f6298b54d123df93af3b0a978
eddyiiz <eddyxsayf@gmail.com>
2024-09-12 11:28:14 +0200

    children routes

--- cashly.client/src/App.jsx
+++ cashly.client/src/App.jsx
@@ -3,17 +3,36 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 import LandingPage from './pages/LandingPage';
-import ErrorPage from './Pages/ErrorPage';
 import HomePage from './Pages/HomePage';
+import ReportsPage from './Pages/ReportsPage';
+import SettingsPage from './Pages/SettingsPage';
+import ErrorPage from './Pages/ErrorPage';
+import MainLayout from './Components/MainLayout';
+
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <LandingPage />,
         errorElement: <ErrorPage />
-    },{
-        path: "/home",
-        element: <HomePage />,
+    },
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: "home",
+                element: <HomePage />,
+            },
+            {
+                path: "reports",
+                element: <ReportsPage />,
+            },
+            {
+                path: "settings",
+                element: <SettingsPage />,
+            },
+        ],
     },
 ]);
 
