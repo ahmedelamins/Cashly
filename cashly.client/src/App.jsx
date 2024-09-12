98010475723bcd1e1d77b902e049ddb59c641307
eddyiiz <eddyxsayf@gmail.com>
2024-09-12 10:15:58 +0200

    Hompage

--- cashly.client/src/App.jsx
+++ cashly.client/src/App.jsx
@@ -4,12 +4,16 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 import LandingPage from './pages/LandingPage';
 import ErrorPage from './Pages/ErrorPage';
+import HomePage from './Pages/HomePage';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <LandingPage />,
         errorElement: <ErrorPage />
+    },{
+        path: "/home",
+        element: <HomePage />,
     },
 ]);
 
