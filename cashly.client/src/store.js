c15871e2bd7f25c28ce0bd9e70a6493a1753251d
eddyiiz <eddyxsayf@gmail.com>
2024-09-15 11:24:06 +0200

    starting with redux toolkit

--- cashly.client/src/store.js
+++ cashly.client/src/store.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducers: {},
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    devTools: true,
+});
+
+export default Store;
