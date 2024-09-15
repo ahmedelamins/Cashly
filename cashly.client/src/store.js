12e45ae272307058c777b6b3c2a3bdc67f25911f
eddyiiz <eddyxsayf@gmail.com>
2024-09-15 13:13:58 +0200

    apiSlice middleware

--- cashly.client/src/store.js
+++ cashly.client/src/store.js
@@ -8,7 +8,7 @@ const store = configureStore({
         auth: authReducer,
         [apiSlice.reducerPath]: apiSlice.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
 });
 
