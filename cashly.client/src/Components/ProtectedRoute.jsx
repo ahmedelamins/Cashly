a19d719bdc6cbf178697b166657801ec9d02f965
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 08:59:13 +0200

    protected routes

---
+++ cashly.client/src/Components/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+        // Redirect to landing/login page if not authenticated
+        return (
+            <Navigate to="/" />
+        );
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
