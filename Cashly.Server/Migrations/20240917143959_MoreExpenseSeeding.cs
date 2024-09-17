c9f26aec3b46739927ce88756024bc4e50fa9342
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 16:42:45 +0200

    more expense seeding

---
+++ Cashly.Server/Migrations/20240917143959_MoreExpenseSeeding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cashly.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoreExpenseSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Expenses",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "UserId",
+                value: 3);
+
+            migrationBuilder.InsertData(
+                table: "Expenses",
+                columns: new[] { "Id", "Amount", "CreatedAt", "Title", "UserId" },
+                values: new object[] { 4, 24.99m, new DateTime(2024, 9, 16, 8, 20, 0, 0, DateTimeKind.Unspecified), "haircut", 3 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Expenses",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Expenses",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "UserId",
+                value: 1);
+        }
+    }
+}
