88ad253cc8a048d2bc4443035aa56306320f4fef
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 15:54:48 +0200

    injecting IExpenseService

---
+++ Cashly.Server/Controllers/expenseConroller.cs
@@ -0,0 +1,17 @@
+﻿using Cashly.Server.Services.ExpenseService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cashly.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class expenseConroller : ControllerBase
+    {
+        private readonly IExpenseService _expenseService;
+
+        public expenseConroller(IExpenseService expenseService)
+        {
+            _expenseService = expenseService;
+        }
+    }
+}
