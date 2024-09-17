5a2b800c58aa02393373d59f2d9a38fe0e1f45cc
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 14:41:11 +0200

    global usings

---
+++ Cashly.Server/Services/ExpenseService/ExpenseService.cs
@@ -0,0 +1,33 @@
+﻿namespace Cashly.Server.Services.ExpenseService;
+
+public class ExpenseService : IExpenseService
+{
+    public ExpenseService(DataContext )
+    {
+
+    }
+    public Task<ServiceResponse<Expense>> CreateExpense(int userId, Expense expense)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<bool>> DeleteExpense(int userId, int expenseId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<Expense>> GetExpenseById(int userId, int expenseId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<List<Expense>>> GetExpenses(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<Expense>> UpdateExpense(int userId, int expenseId, Expense updatedExpense)
+    {
+        throw new NotImplementedException();
+    }
+}
