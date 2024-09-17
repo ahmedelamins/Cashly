b75617f156b096a9185ab0293be408c6ec74585c
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 16:11:32 +0200

    update and delete

--- Cashly.Server/Controllers/expenseConroller.cs
+++ Cashly.Server/Controllers/expenseConroller.cs
@@ -65,5 +65,28 @@ namespace Cashly.Server.Controllers
             return Ok(response);
         }
 
+        [HttpPut("{id:int}"), Authorize]
+        public async Task<ActionResult<ServiceResponse<Expense>>> UpdateExpense(int Id, [FromBody] Expense UpdatedExpense)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var response = await _expenseService.UpdateExpense(userId, Id, UpdatedExpense);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id:int}"), Authorize]
+        public async Task<ActionResult<ServiceResponse<bool>>> DeleteExpense(int id)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var response = await _expenseService.DeleteExpense(userId, id);
+
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
     }
 }
