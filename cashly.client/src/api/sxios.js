5db64252fc1e8d13835556d2c5b8ee790673e4a9
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 06:20:15 +0200

    authorization header

--- cashly.client/src/api/sxios.js
+++ cashly.client/src/api/sxios.js
@@ -7,4 +7,16 @@ const axiosInstance = axios.create({
     },
 });
 
+//Authorization header for all authenticated requests
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+
 export default axiosInstance;
