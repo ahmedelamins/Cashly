d389c0a4540fd2ddb8fdc9e9a5699d82e331d689
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 14:53:29 +0200

    default message

--- Cashly.Server/Models/ServiceResponse.cs
+++ Cashly.Server/Models/ServiceResponse.cs
@@ -3,6 +3,6 @@
 public class ServiceResponse<T>
 {
     public T? Data { get; set; }
-    public string Message { get; set; } = string.Empty;
+    public string Message { get; set; } = "Successful";
     public bool Success { get; set; } = true;
 }
