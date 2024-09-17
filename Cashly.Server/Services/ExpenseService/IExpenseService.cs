d9a26b90cf778edf3b1d15988caa0198b5128c82
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 14:39:08 +0200

    interface

---
+++ Cashly.Server/Services/ExpenseService/IExpenseService.cs
@@ -0,0 +1,10 @@
+﻿namespace Cashly.Server.Services.ExpenseService;
+
+public interface IExpenseService
+{
+    Task<ServiceResponse<List<Expense>>> GetExpenses(int userId);
+    Task<ServiceResponse<Expense>> GetExpenseById(int userId, int expenseId);
+    Task<ServiceResponse<Expense>> CreateExpense(int userId, Expense expense);
+    Task<ServiceResponse<Expense>> UpdateExpense(int userId, int expenseId, Expense updatedExpense);
+    Task<ServiceResponse<bool>> DeleteExpense(int userId, int expenseId);
+}
