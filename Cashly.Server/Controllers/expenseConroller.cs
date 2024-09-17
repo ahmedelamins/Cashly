864187b5f3ad4744d0a091b3351d7d0f75b55e84
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 16:04:18 +0200

    get expenses route

--- Cashly.Server/Controllers/expenseConroller.cs
+++ Cashly.Server/Controllers/expenseConroller.cs
@@ -1,5 +1,7 @@
 ﻿using Cashly.Server.Services.ExpenseService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Cashly.Server.Controllers
 {
@@ -13,5 +15,27 @@ namespace Cashly.Server.Controllers
         {
             _expenseService = expenseService;
         }
+
+        [HttpGet, Authorize]
+        public async Task<ActionResult<ServiceResponse<List<Expense>>>> GetExpenses()
+        {
+            try
+            {
+                var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                var response = await _expenseService.GetExpenses(userId);
+
+                if (!response.Success)
+                {
+                    return BadRequest(response);
+                }
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
     }
 }
