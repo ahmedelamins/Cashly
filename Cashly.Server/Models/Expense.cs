d68f0ef0969bd9189e925bf5f5a5fbf901e51ede
eddyiiz <eddyxsayf@gmail.com>
2024-09-17 13:12:37 +0200

    expense model

---
+++ Cashly.Server/Models/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cashly.Server.Models;
+public class Expense
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Amount { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int UserId { get; set; }
+    [Required]
+    public User User { get; set; }
+}
