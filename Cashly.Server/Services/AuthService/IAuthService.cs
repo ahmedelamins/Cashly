0716ae5626944cb15a066a59cf16d4b965de696f
eddyiiz <eddyxsayf@gmail.com>
2024-09-18 10:43:15 +0200

    change password

--- Cashly.Server/Services/AuthService/IAuthService.cs
+++ Cashly.Server/Services/AuthService/IAuthService.cs
@@ -5,4 +5,5 @@ public interface IAuthService
     Task<ServiceResponse<int>> Register(User user, string password);
     Task<ServiceResponse<string>> Login(string username, string password);
     Task<ServiceResponse<bool>> DeleteUser(int userId);
+    Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword);
 }
