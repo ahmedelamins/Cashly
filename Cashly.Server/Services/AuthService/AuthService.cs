b025b7fe8677dc98ba6a02277ab6706913ac2042
eddyiiz <eddyxsayf@gmail.com>
2024-09-18 10:45:46 +0200

    password change method

--- Cashly.Server/Services/AuthService/AuthService.cs
+++ Cashly.Server/Services/AuthService/AuthService.cs
@@ -79,6 +79,42 @@ public class AuthService : IAuthService
         return response;
     }
 
+    public async Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword)
+    {
+        var response = new ServiceResponse<bool>();
+        try
+        {
+            var user = await _context.Users.FindAsync(userId);
+
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "Not Found";
+
+                return response;
+            }
+
+            CreatePasswordHash(newPassword, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordSalt = passwordSalt;
+            user.PasswordHash = passwordHash;
+
+            await _context.SaveChangesAsync();
+
+
+            response.Message = "Password Changed Successfully";
+            response.Data = true;
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+
     public async Task<ServiceResponse<bool>> DeleteUser(int userId)
     {
         var response = new ServiceResponse<bool>();
@@ -157,7 +193,7 @@ public class AuthService : IAuthService
         var token = new JwtSecurityToken
             (
               claims: claims,
-              expires: DateTime.Now.AddDays(1),
+              expires: DateTime.Now.AddHours(1),
               signingCredentials: creds
             );
 
@@ -165,4 +201,5 @@ public class AuthService : IAuthService
 
         return jwt;
     }
+
 }
