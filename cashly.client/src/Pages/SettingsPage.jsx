86f664e4e0b45d3021a832d518f39597970de663
eddyiiz <eddyxsayf@gmail.com>
2024-09-19 22:03:10 +0200

    handleDeleteAccount init

--- cashly.client/src/Pages/SettingsPage.jsx
+++ cashly.client/src/Pages/SettingsPage.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import { Container, Typography, Box, Button } from '@mui/material';
+import axios from 'axios';
+import { toast } from "react-toastify";
 
 const SettingsPage = () => {
-    const handleChangePassword = () => {
+    const handleChangePassword = (e) => {
         console.log('Change Password Clicked');
     };
 
-    const handleDeleteAccount = () => {
-        console.log('Delete Account Clicked');
+    const handleDeleteAccount = async (e) => {
+        e.preventDefault();
+
+        try {
+            toast.warning("ou are about to Delete Your Account!")
+
+        } catch {
+            toast.error("Something went wrong")
+        }
     };
 
     return (
         <Container>
-            <Box sx={{ mt: 4}}>
-                <Typography variant="h4" gutterBottom sx={{ mb: 2 }}>
+            <Box sx={{ mt: 5}}>
+                <Typography variant="h4" gutterBottom sx={{ mb: 3 }}>
                     Manage Your Account
                 </Typography>
-                <Box sx={{ mt: 4, display: 'flex', maxWidth: "250px", width: "100%", flexDirection: 'column', w: "40%" }}>
+                <Box sx={{ mt: 4, display: 'flex', maxWidth: "300px", width: "100%", flexDirection: 'column', w: "40%" }}>
                     <Button
                         variant="contained"
                         color="primary"
