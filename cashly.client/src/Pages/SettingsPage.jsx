74852bcf071f28dee1a7d36c94d8156f542463c1
eddyiiz <eddyxsayf@gmail.com>
2024-09-19 20:03:39 +0200

    init settings screen

--- cashly.client/src/Pages/SettingsPage.jsx
+++ cashly.client/src/Pages/SettingsPage.jsx
@@ -1,6 +1,40 @@
+import React from 'react';
+import { Container, Typography, Box, Button } from '@mui/material';
+
 const SettingsPage = () => {
+    const handleChangePassword = () => {
+        console.log('Change Password Clicked');
+    };
+
+    const handleDeleteAccount = () => {
+        console.log('Delete Account Clicked');
+    };
+
     return (
-        <h1>This is Settings</h1>
+        <Container>
+            <Box sx={{ mt: 4 }}>
+                <Typography variant="h4" gutterBottom sx={{ mb: 2} }>
+                    Manage Your Account
+                </Typography>
+                <Box sx={{ mt: 2, display: 'flex', maxWidth: "250px", width: "100%", flexDirection: 'column', w: "40%" }}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleChangePassword}
+                        sx={{ mb: 2 }}
+                    >
+                        Change Password
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="error"
+                        onClick={handleDeleteAccount}
+                    >
+                        Delete Account
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
     );
 }
 
