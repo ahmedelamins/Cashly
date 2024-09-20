e631cc1f85f923512832fbe331d44254445f92ae
eddyiiz <eddyxsayf@gmail.com>
2024-09-20 11:13:41 +0200

    change password dto

---
+++ Cashly.Server/Models/ChangePassword.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cashly.Server.Models;
+public class ChangePassword
+{
+    [Required, StringLength(20, MinimumLength = 4)]
+    public string Password { get; set; } = string.Empty;
+}
