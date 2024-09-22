43389d3bf1cdb370d613e7ac990aa623b24a0ce9
eddyiiz <eddyxsayf@gmail.com>
2024-09-22 10:27:27 +0200

    establishing 1=>n (category)

--- Cashly.Server/Data/DataContext.cs
+++ Cashly.Server/Data/DataContext.cs
@@ -11,41 +11,46 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
             .WithOne(e => e.User)
             .HasForeignKey(e => e.UserId);
 
-        //seeding some expenses
-        modelBuilder.Entity<Expense>().HasData(
+        modelBuilder.Entity<Category>()
+            .HasMany(c => c.Expenses)
+            .WithOne(c => c.Category)
+            .HasForeignKey(c => c.CategoryId);
 
-             new Expense
-             {
-                 Id = 1,
-                 Title = "Lunch",
-                 Amount = 12.99m,
-                 CreatedAt = new DateTime(2024, 9, 17, 12, 30, 0),
-                 UserId = 1 // belongs to user 1
-             },
-             new Expense
-             {
-                 Id = 2,
-                 Title = "shopping",
-                 Amount = 99.99m,
-                 CreatedAt = new DateTime(2024, 9, 17, 1, 30, 0),
-                 UserId = 1
-             },
-             new Expense
-             {
-                 Id = 3,
-                 Title = "cinema",
-                 Amount = 5.99m,
-                 CreatedAt = new DateTime(2024, 9, 17, 2, 20, 0),
-                 UserId = 3
-             },
-             new Expense
-             {
-                 Id = 4,
-                 Title = "haircut",
-                 Amount = 24.99m,
-                 CreatedAt = new DateTime(2024, 9, 16, 8, 20, 0),
-                 UserId = 3
-             }
-        );
+        //seeding some expenses
+        //modelBuilder.Entity<Expense>().HasData(
+
+        //     new Expense
+        //     {
+        //         Id = 1,
+        //         Title = "Lunch",
+        //         Amount = 12.99m,
+        //         CreatedAt = new DateTime(2024, 9, 17, 12, 30, 0),
+        //         UserId = 1 // belongs to user 1
+        //     },
+        //     new Expense
+        //     {
+        //         Id = 2,
+        //         Title = "shopping",
+        //         Amount = 99.99m,
+        //         CreatedAt = new DateTime(2024, 9, 17, 1, 30, 0),
+        //         UserId = 1
+        //     },
+        //     new Expense
+        //     {
+        //         Id = 3,
+        //         Title = "cinema",
+        //         Amount = 5.99m,
+        //         CreatedAt = new DateTime(2024, 9, 17, 2, 20, 0),
+        //         UserId = 3
+        //     },
+        //     new Expense
+        //     {
+        //         Id = 4,
+        //         Title = "haircut",
+        //         Amount = 24.99m,
+        //         CreatedAt = new DateTime(2024, 9, 16, 8, 20, 0),
+        //         UserId = 3
+        //     }
+        //);
     }
 }
