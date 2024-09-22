374b66883e750d1bc2c79d3b5c296c86e4342c15
eddyiiz <eddyxsayf@gmail.com>
2024-09-22 11:26:53 +0200

    categories migration

---
+++ Cashly.Server/Migrations/20240922092605_CategoriesUpdated.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cashly.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoriesUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Shopping");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Savings");
+        }
+    }
+}
