33fdaf98139295aeb7e3d046f2727e2c76e450b1
eddyiiz <eddyxsayf@gmail.com>
2024-09-22 17:21:18 +0200

    categories table

--- Cashly.Server/Data/DataContext.cs
+++ Cashly.Server/Data/DataContext.cs
@@ -4,6 +4,7 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Expense> Expenses { get; set; }
+    public DbSet<Category> Categories { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>()
