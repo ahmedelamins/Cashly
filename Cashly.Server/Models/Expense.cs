6eb41a2f7a986e48f1e61998936b0a6c83103544
eddyiiz <eddyxsayf@gmail.com>
2024-09-22 10:21:46 +0200

    include category

--- Cashly.Server/Models/Expense.cs
+++ Cashly.Server/Models/Expense.cs
@@ -8,8 +8,11 @@ public class Expense
     public string Title { get; set; } = string.Empty;
     [Column(TypeName = "decimal(18,2)")]
     public decimal Amount { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    [Required]
+    public Category Category { get; set; }
+    public int CategoryId { get; set; }
     [Required]
     public User User { get; set; }
     public int UserId { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
