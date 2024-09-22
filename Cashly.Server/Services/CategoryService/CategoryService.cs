a3b25276d083c07f7a7b18e06b5f951892152244
eddyiiz <eddyxsayf@gmail.com>
2024-09-22 16:29:12 +0200

    init

--- Cashly.Server/Services/CategoryService/CategoryService.cs
+++ Cashly.Server/Services/CategoryService/CategoryService.cs
@@ -1,5 +1,10 @@
-﻿namespace Cashly.Server.Services.CategoryService;
+﻿
+namespace Cashly.Server.Services.CategoryService;
 
 public class CategoryService : ICategoryService
 {
+    public Task<ServiceResponse<List<Category>>> GetCategories()
+    {
+        throw new NotImplementedException();
+    }
 }
