7157d8a29204995df46263c2fce4a484b69a0710
eddyiiz <eddyxsayf@gmail.com>
2024-09-22 10:27:12 +0200

    expenses navigation

--- Cashly.Server/Models/Category.cs
+++ Cashly.Server/Models/Category.cs
@@ -4,4 +4,5 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; } = "General";
+    public List<Expense> Expenses { get; set; } = new List<Expense>();
 }
