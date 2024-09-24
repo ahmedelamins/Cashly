ea14b8727f697d5df7d3c8376ff85779d78ac2cf
eddyiiz <eddyxsayf@gmail.com>
2024-09-24 10:25:44 +0200

    new expense form dialog

--- cashly.client/src/Pages/HomePage.jsx
+++ cashly.client/src/Pages/HomePage.jsx
@@ -39,6 +39,12 @@ const HomePage = () => {
         setOpenAddExpense(false);
     }
 
+    const handleAddExpenseSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("new expense: ", formData);
+    };
+
     return (
         <Box sx={{ mt: 1, mb: 2, p: 1 }} >
             <Typography variant="h4">
@@ -46,6 +52,7 @@ const HomePage = () => {
             </Typography>
             <Button
                 variant="contained"
+                onClick={handleOpenAddExpense}
                 sx={{
                     mt: 3,
                     fontWeight: "540",
@@ -58,7 +65,71 @@ const HomePage = () => {
                 }}>
                 New Expense
             </Button>            
-
+            {/* new expense form dialog*/}
+            <Dialog open={openAddExpense} onClose={handleCloseAddExpense}>
+                <DialogTitle>New Expense</DialogTitle>
+                <DialogContent>
+                    <Box component="form" onSubmit={handleAddExpenseSubmit}>
+                        <TextField
+                            margin="dense"
+                            name="title"
+                            label="Title"
+                            type="text"
+                            fullWidth
+                            value={formData.title}
+                            onChange={handleChange}
+                            required
+                        />
+                        <TextField
+                            margin="dense"
+                            name="amount"
+                            label="Amount"
+                            type="number"
+                            fullWidth
+                            required
+                            value={formData.amount}
+                            onChange={handleChange}
+                        />      
+                        <TextField
+                            margin="dense"
+                            name="category"
+                            label="Category"
+                            type="text"
+                            //select
+                            fullWidth
+                            variant="outlined"
+                            value={formData.category}
+                            onChange={handleChange}
+                            required
+                        />
+                        <TextField
+                            margin="dense"
+                            name="date"
+                            label="Date"
+                            type="date"
+                            //fullWidth
+                            variant="outlined"
+                            InputLabelProps={{ shrink: true }}
+                            value={formData.date}
+                            onChange={handleChange}
+                            required
+                        />                        
+                        <DialogActions>
+                        <Button
+                                variant="outlined"
+                                onClick={handleCloseAddExpense}>
+                            
+                                Discard
+                            </Button>
+                            <Button
+                                variant="contained"
+                                type="submit">
+                                submit
+                            </Button>
+                        </DialogActions>
+                    </Box>
+                </DialogContent>
+            </Dialog>
         </Box>
     );
 }
