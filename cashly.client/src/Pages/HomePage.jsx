86d11040096d5c056d554af45de73ad8b5cb0e02
eddyiiz <eddyxsayf@gmail.com>
2024-09-24 08:51:20 +0200

    greetin animation

--- cashly.client/src/Pages/HomePage.jsx
+++ cashly.client/src/Pages/HomePage.jsx
@@ -5,10 +5,19 @@ const HomePage = () => {
     const Username = username.charAt(0).toUpperCase() + username.slice(1); //Capitalize first letter
 
     return (
-        <Box sx={{ mt: 1, mb: 1, p: 1  }} >
-            <Typography variant="h4">
-                Hello, {Username}!
-            </Typography>
+        <Box sx={{ mt: 1, mb: 1, p: 1 }} >
+            <Box
+                sx={{
+                    animation: 'slideIn 1s ease-out',
+                    '@keyframes slideIn': {
+                        '0%': { transform: 'translateY(20px)', opacity: 0 },
+                        '100%': { transform: 'translateY(0)', opacity: 1 },
+                    }
+                }}>
+                <Typography variant="h4">
+                    Hello, {Username}!
+                </Typography>
+            </Box>
 
         </Box>
     );
