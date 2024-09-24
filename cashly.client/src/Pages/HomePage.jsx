76913f848b5e4bc1299da95fc62d1f70d10df0e9
eddyiiz <eddyxsayf@gmail.com>
2024-09-24 09:47:40 +0200

    new expense button styling

--- cashly.client/src/Pages/HomePage.jsx
+++ cashly.client/src/Pages/HomePage.jsx
@@ -1,31 +1,33 @@
-import { Box, Typography } from '@mui/material';
+import {
+    Box,
+    Typography,
+    Button
+} from '@mui/material';
 
 const HomePage = () => {
     const username = localStorage.getItem('username'); // Fetching username
     const Username = username.charAt(0).toUpperCase() + username.slice(1); //Capitalize first letter
 
     return (
-        <Box sx={{ mt: 1, mb: 1, p: 1 }} >
-            <Box
+        <Box sx={{ mt: 1, mb: 2, p: 1 }} >
+            <Typography variant="h4">
+
+                Hello, {Username}!
+            </Typography>
+            <Button
+                variant="contained"
                 sx={{
-                    maxWidth: '300px',
-                    padding: '20px',
-                    textAlign: 'center',
-                    backgroundColor: '#f0f4f8', 
-                    borderRadius: '8px',
-                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', 
-                    animation: 'fadeSlideUp 0.8s ease-in-out',
-                    '@keyframes fadeSlideUp': {
-                        '0%': { opacity: 0, transform: 'translateY(10px)' },
-                        '100%': { opacity: 1, transform: 'translateY(0)' },
-                    }
+                    mt: 3,
+                    fontWeight: "500",
+                    py: 1.5,
+                    px: 3,
+                    transition: 'transform 0.3s ease',
+                    '&:hover': {
+                        transform: 'scale(1.1)',
+                    },
                 }}>
-                <Typography variant="h4">
-                    Hello, {Username}!
-                </Typography>
-            </Box>
-
-
+                New Expense
+            </Button>            
 
         </Box>
     );
