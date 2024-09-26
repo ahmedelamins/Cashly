591b5aa6c99fe8f1389e3dc7466d537ade7393fb
eddyiiz <eddyxsayf@gmail.com>
2024-09-26 17:55:51 +0200

    removed required

--- Cashly.Server/Models/ChangePassword.cs
+++ Cashly.Server/Models/ChangePassword.cs
@@ -1,8 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Cashly.Server.Models;
+﻿namespace Cashly.Server.Models;
 public class ChangePassword
 {
-    [Required, StringLength(20, MinimumLength = 4)]
     public string Password { get; set; } = string.Empty;
 }
