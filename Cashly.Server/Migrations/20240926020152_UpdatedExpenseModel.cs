3d1d4e8eaccb4a4e286d3d7a898b9df7866c6550
eddyiiz <eddyxsayf@gmail.com>
2024-09-26 04:06:12 +0200

    adjusting expense table

---
+++ Cashly.Server/Migrations/20240926020152_UpdatedExpenseModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cashly.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedExpenseModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Expenses",
+                newName: "Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Expenses",
+                newName: "CreatedAt");
+        }
+    }
+}
