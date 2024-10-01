c2b2efe5130a04963a30c5dc49a39b9cdaca320f
eddyiiz <eddyxsayf@gmail.com>
2024-10-01 07:09:22 +0200

    TotalExpenses

--- Cashly.Server/Services/ReportService/ReportService.cs
+++ Cashly.Server/Services/ReportService/ReportService.cs
@@ -2,4 +2,36 @@
 
 public class ReportService : IReportService
 {
+    private readonly DataContext _context;
+
+    public ReportService(DataContext context)
+    {
+        _context = context;
+    }
+    public async Task<ServiceResponse<decimal>> GetTotalExpenses(int userId, DateOnly? startDate = null, DateOnly? endDate = null)
+    {
+        var response = new ServiceResponse<decimal>();
+
+        try
+        {
+            var totalExpenses = await _context.Expenses
+                .Where(e => e.UserId == userId)
+                .SumAsync(e => e.Amount);
+
+            response.Data = totalExpenses;
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+    public Task<ServiceResponse<Dictionary<string, decimal>>> GetExpensesByCategory(int userId, DateOnly? startDate = null, DateOnly? endDate = null)
+    {
+        throw new NotImplementedException();
+    }
+
 }
