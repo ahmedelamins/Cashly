9ed99a1cc8387a8558a69147aa376f94c72976f2
eddyiiz <eddyxsayf@gmail.com>
2024-10-01 12:34:30 +0200

    GetExpenseByCategory

--- Cashly.Server/Services/ReportService/ReportService.cs
+++ Cashly.Server/Services/ReportService/ReportService.cs
@@ -15,7 +15,9 @@ public class ReportService : IReportService
         try
         {
             var totalExpenses = await _context.Expenses
-                .Where(e => e.UserId == userId)
+                .Where(e => e.UserId == userId &&
+                                 (!startDate.HasValue || e.Date >= startDate.Value) &&
+                                 (!endDate.HasValue || e.Date <= endDate.Value))
                 .SumAsync(e => e.Amount);
 
             response.Data = totalExpenses;
@@ -29,9 +31,33 @@ public class ReportService : IReportService
 
         return response;
     }
-    public Task<ServiceResponse<Dictionary<string, decimal>>> GetExpensesByCategory(int userId, DateOnly? startDate = null, DateOnly? endDate = null)
+    public async Task<ServiceResponse<Dictionary<string, decimal>>> GetExpensesByCategory(int userId, DateOnly? startDate = null, DateOnly? endDate = null)
     {
-        throw new NotImplementedException();
+        var response = new ServiceResponse<Dictionary<string, decimal>>();
+
+        try
+        {
+            var expenseByCategory = await _context.Expenses
+                .Where(e => e.UserId == userId &&
+                                 (!startDate.HasValue || e.Date >= startDate.Value) &&
+                                 (!endDate.HasValue || e.Date <= endDate.Value))
+                .GroupBy(e => e.Category)
+                .Select(g => new
+                {
+                    Category = g.Key,
+                    TotalAmount = g.Sum(e => e.Amount)
+                })
+               .ToDictionaryAsync(g => g.Category, g => g.TotalAmount);
+
+            response.Data = expenseByCategory;
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
     }
 
 }
