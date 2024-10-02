070b1aaca7d5fe7ce420c08870184f3c19cc5942
eddyiiz <eddyxsayf@gmail.com>
2024-10-02 04:16:30 +0200

    init

--- cashly.client/src/Pages/ReportsPage.jsx
+++ cashly.client/src/Pages/ReportsPage.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from 'react';
+import {
+    Box,
+    Typography,
+    Button,
+    Card,
+    Paper,
+    Grid,
+} from '@mui/material';
+
 const ReportsPage = () => {
 
     return (
-        <h1>This is Reports</h1>
+        <Box sx={{ mt: 1, mb: 2, p: 1 }}>
+            <Typography variant="h4" sx={{ textAlign: 'left' }}>
+                Understand your expenses
+            </Typography>
+
+            {/* report container*/}
+            <Grid item xs={1} md={6} sx={{ mt: 2 }}>
+                <Paper elevation={3} sx={{ p: 2, textAlign: 'center' }}>
+                    <Typography variant="h6" sx={{ mb: 2 }}>
+                        Expenditure by category
+                    </Typography>
+                    
+                </Paper>
+            </Grid>
+        </Box>
     );
 }
 
