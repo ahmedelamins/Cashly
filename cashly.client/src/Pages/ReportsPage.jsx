20fef311db93228d5dd223ad504de0f84cdacaa6
eddyiiz <eddyxsayf@gmail.com>
2024-10-02 08:30:19 +0200

    init

--- cashly.client/src/Pages/ReportsPage.jsx
+++ cashly.client/src/Pages/ReportsPage.jsx
@@ -12,19 +12,10 @@ const ReportsPage = () => {
 
     return (
         <Box sx={{ mt: 1, mb: 2, p: 1 }}>
-            <Typography variant="h4" sx={{ textAlign: 'left' }}>
+            <Typography variant="h4" gutterBottom sx={{ letterSpacing: '0.1em', textAlign: 'left' }}>
                 Understand your expenses
             </Typography>
 
-            {/* report container*/}
-            <Grid item xs={1} md={6} sx={{ mt: 2 }}>
-                <Paper elevation={3} sx={{ p: 2, textAlign: 'center' }}>
-                    <Typography variant="h6" sx={{ mb: 2 }}>
-                        Expenditure by category
-                    </Typography>
-                    
-                </Paper>
-            </Grid>
         </Box>
     );
 }
