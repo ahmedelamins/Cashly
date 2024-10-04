6cc8a2febd5f184d050a2735dc16f517301571b0
eddyiiz <eddyxsayf@gmail.com>
2024-10-04 07:34:56 +0200

    GetMonthlyExpenses

--- Cashly.Server/Services/ReportService/IReportService.cs
+++ Cashly.Server/Services/ReportService/IReportService.cs
@@ -4,5 +4,5 @@ public interface IReportService
     Task<ServiceResponse<decimal>> GetTotalExpenses(int userId);
     Task<ServiceResponse<decimal>> GetAverageSpendingPerCategory(int userId);
     Task<ServiceResponse<string>> GetMosExpensiveCategory(int userId);
-    Task<ServiceResponse<List<decimal>>> GetWeeklyExpenses(int userId);
+    Task<ServiceResponse<List<decimal>>> GetMonthlyExpenses(int userId);
 }
